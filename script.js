966b95a6718f742e4331a9a9cca74593ba142b4d
Tomás <tomas.ep22@gmail.com>
2021-03-04 20:40:23 -0300

    save2

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ function createGrid() {
     const el = document.createElement('div');
     container.appendChild(el);
     el.classList.add('square');
+    el.classList.add(`square${num}`);
     el.textContent = num;
     num++
 }
@@ -16,3 +17,13 @@ for (var i = 1; i <= 256; ++i) {
     createGrid(i);
 }
 
+function addActive(e) {
+ console.log(this)
+}
+
+function removeActive(e) {
+
+}
+
+squares.forEach(square => square.addEventListener('mouseenter', addActive));
+squares.forEach(square => square.addEventListener('mouseleave', removeActive));
