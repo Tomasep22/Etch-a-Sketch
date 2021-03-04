3ac5b00a8c1dc5c6c9a54dfd12ecb9a9e531e60e
tomasep22 <tomas.ep22@gmail.com>
2021-03-04 16:57:53 -0300

    save progress

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 const container = document.querySelector('.container');
 
+let num = 1;
+
+function createGrid() {
+    
+    // adds 1 square each time it runs
+    const el = document.createElement('div');
+    container.appendChild(el);
+    el.classList.add('square');
+    el.textContent = num;
+    num++
+}
+
+for (var i = 1; i <= 256; ++i) {
+    createGrid(i);
+}
 
