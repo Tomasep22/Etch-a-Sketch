f15c9244f0224964de7d18e3f5bb6815d292e340
tomasep22 <tomas.ep22@gmail.com>
2021-03-06 02:31:02 -0300

    First 'stable' version

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
 const container = document.querySelector('.container');
+const clear = document.querySelector('.clear');
 
 let num = 1;
+let numSquares = 256;
 
-function createGrid() {
+function addSquare() {
     
     // adds 1 square each time it runs
     const el = document.createElement('div');
     container.appendChild(el);
     el.classList.add('square');
     el.classList.add(`square${num}`);
-    el.textContent = num;
     num++
+    const squares = container.querySelectorAll('.square');
+    squares.forEach(square => square.addEventListener('mouseenter', addActive));
 }
 
-for (var i = 1; i <= 256; ++i) {
-    createGrid(i);
+for (var i = 1; i <= numSquares; ++i) {
+    addSquare(i);
 }
 
-function addActive(e) {
- console.log(this)
+function addActive() {
+this.classList.add('active');
 }
 
-function removeActive(e) {
-
+function newGrid(e) {
+  const squares = container.querySelectorAll('.square');
+  squares.forEach(square => container.removeChild(square))
+  const rows = prompt('Rows', 'Input number of rows for the new grid');
+  const columns = prompt('Columns', 'Input number of rows for the new grid');
+  console.log(rows, columns);
+  numSquares = rows * columns
+  for (var i = 1; i <= numSquares; ++i) {
+    addSquare(i);
+    };
+  document.documentElement.style.setProperty('--rows', `repeat(${rows}, 1fr)`);
+  document.documentElement.style.setProperty('--columns', `repeat(${columns}, 1fr)`);
 }
 
-squares.forEach(square => square.addEventListener('mouseenter', addActive));
-squares.forEach(square => square.addEventListener('mouseleave', removeActive));
+clear.addEventListener('click', newGrid);
+    
+
